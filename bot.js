d2d54d1a686e6fbbc8908678fc2e5382c6097b02
Nathaniel Dunham <dun19007@byui.edu>
2023-08-28 19:32:53 -0700

    implemented environmental variables

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const token = 'MTE0NTg5Nzg3NzMwNzEzODEyOA.Gdwsg9.GKYUkEpW2BMgY8PERjWIIxs_8b_8put5Al19iE'; // Replace with your bot token
+const token = process.env.BOT_TOKEN; // Replace with your bot token
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
